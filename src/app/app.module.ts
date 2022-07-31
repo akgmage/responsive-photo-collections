bc413d6bba542aa8f699dd2acbf565b23dae45dc
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-31 22:45:59 +0530

    remove reference for tryouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
-import { TryoutsComponent } from './tryouts/tryouts.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, TryoutsComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
