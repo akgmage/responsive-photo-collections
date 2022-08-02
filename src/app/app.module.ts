2711d434019375959929d25b77bd3528b68316e6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-02 08:54:49 +0530

    add popular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { BannerComponent } from './banner/banner.component';
+import { PopularComponent } from './popular/popular.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, BannerComponent],
+  declarations: [AppComponent, HomeComponent, BannerComponent, PopularComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
